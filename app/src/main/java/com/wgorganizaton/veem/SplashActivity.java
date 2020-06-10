caa5f21ecb6ee06c73ba0b773dc57f48b5672d53
saadbutt7836 <saadbutt7836@gmail.com>
2020-06-10 00:00:59 -0700

    Initial commit

---
+++ app/src/main/java/com/wgorganizaton/veem/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.wgorganizaton.veem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
